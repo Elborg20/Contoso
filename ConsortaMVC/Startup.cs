ad3f092262aaa1833aa485f6fefa06bd80b508dc
elborg20 <mtgroble@gmail.com>
2018-12-09 21:38:09 -0700

    First commit

---
+++ ConsortaMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConsortaMVC.Startup))]
+namespace ConsortaMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
